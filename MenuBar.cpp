170dda49fb953b9826977255c85091acaf982ca2
Andy <fox939@hotmail.com>
2012-10-25 16:08:52 +0100

    Starting Point Libraries included, taglib working, soundtouch_source.cpp needs to be included but isn't due to cpu_detect.h problems Beginning work on incorporating audio player function, adding controls etc

---
+++ MenuBar.cpp
@@ -0,0 +1,11 @@
+/*
+ *  MenuBar.cpp
+ *  MusicPlayer
+ *
+ *  Created by Andy on 25/10/2012.
+ *  Copyright 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "MenuBar.h"
+
---
+++ MenuBar.h
@@ -0,0 +1,25 @@
+#ifndef MENUBAR
+#define MENUBAR
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+
+//==============================================================================
+/*
+
+ */
+class MenuBar  : public MenuBarModel
+{
+public:
+    //==============================================================================
+    MenuBar();
+    ~MenuBar();
+	
+	
+private:
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MenuBar)
+};
+
+
+#endif  //MENUBAR
